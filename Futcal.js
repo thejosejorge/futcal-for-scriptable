5c5d3b96c83616c69d3252b891d9c635e75bfe6a
Evan Fox <43059689+thisisevanfox@users.noreply.github.com>
2021-01-31 10:03:28 +0100

    chore(widget): Formatting

--- Futcal.js
+++ Futcal.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: deep-green; icon-glyph: null;
+// icon-color: deep-green; icon-glyph: futbol;
 
 // Widget customisation
 const defaultSettings = {
