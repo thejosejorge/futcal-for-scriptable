129b4ba723e3985de01fdfbd53dcd3ebb90aa88e
thejosejorge <thejosejorge@gmail.com>
2021-06-10 18:55:10 +0100

    Update Futcal.js
    Quick fix for FotMob API Date/Time change

--- Futcal.js
+++ Futcal.js
@@ -287,10 +287,10 @@ async function addWidgetMatch(matchesStack, match, title) {
                 addFormattedText(matchInfoDetailsStack, detailsCancellationValue, Font.regularSystemFont(12), Color.gray(), null, false);
             } else {
                 // If match is in the future show date and time
-                const detailsDateValue = formatDate(new Date((matchDetails.content.matchFacts.infoBox["Match Date"]).replaceAll(".", "")));
+                const detailsDateValue = formatDate(new Date((matchDetails.content.matchFacts.infoBox["Match Date"].dateFormatted).replaceAll(".", "")));
                 addFormattedText(matchInfoDetailsStack, detailsDateValue, Font.regularSystemFont(12), Color.gray(), null, false);
                 matchInfoDetailsStack.addSpacer(3);
-                const detailsTimeValue = formatTime(new Date((matchDetails.content.matchFacts.infoBox["Match Date"]).replaceAll(".", "")));
+                const detailsTimeValue = formatTime(new Date((`${matchDetails.content.matchFacts.infoBox["Match Date"].dateFormatted} ${matchDetails.content.matchFacts.infoBox["Match Date"].timeFormatted}`).replaceAll(".", "")));
                 addFormattedText(matchInfoDetailsStack, detailsTimeValue, Font.regularSystemFont(12), Color.gray(), null, false);
             }
         } else {
